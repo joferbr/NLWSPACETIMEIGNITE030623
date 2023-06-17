9c3f3831476972dfce46c96a7c710b5a6191ebbc
Jose Fernandes <jofer.br@gmail.com>
2023-06-16 23:32:59 -0300

    Commit em 1605232332

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+
+const app = fastify()
+
+app.register(cors, {
+  // origin: ['http://rocketseat.com.br/'], 
+  // Acesso todo frontend. Em produção é diferente.
+
+  origin: true,
+})
+
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
